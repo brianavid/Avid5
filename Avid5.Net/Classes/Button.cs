25f2205c1c8c389c44842e1fca05411fd107c2c6
brianavid <collins.bj@gmail.com>
2023-09-10 19:37:18 +0100

    Simplify button naming and classification in preparation for move to CSS

---
+++ Avid5.Net/Classes/Button.cs
@@ -0,0 +1,29 @@
+﻿using SpotifyAPI.Web;
+
+public class Button
+{
+    static string Image(string buttonName)
+    {
+        return $"<img src='/Content/Buttons/{buttonName}.png'/>";
+    }
+    public static string SmallRound(string buttonName)
+    {
+        return Image("SmallRound/" + buttonName);
+    }
+    public static string BigRound(string buttonName)
+    {
+        return Image("BigRound/" + buttonName);
+    }
+    public static string MiniRound(string buttonName)
+    {
+        return Image("MiniRound/" + buttonName);
+    }
+    public static string Square(string buttonName)
+    {
+        return Image("Square/" + buttonName);
+    }
+    public static string Rect(string buttonName)
+    {
+        return Image(buttonName);
+    }
+}
