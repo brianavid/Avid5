a711150490756394b892ade4ef83d80664f5dcea
brianavid <collins.bj@gmail.com>
2023-09-24 12:18:39 +0100

    Add (on a branch) logging for JRMC delays and capturing of JRMC logs after such a delay

--- Avid5.Net/Controllers/MusicController.cs
+++ Avid5.Net/Controllers/MusicController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NLog;
@@ -89,6 +90,8 @@ namespace Avid5.Net.Controllers
             return PartialView();
         }
 
+        static DateTime timeStartedPlaying = DateTime.MinValue;
+
         // GET: /Music/GetPlayingInfo
         public ContentResult GetPlayingInfo()
         {
@@ -98,14 +101,81 @@ namespace Avid5.Net.Controllers
             {
                 if (info.HasElements)
                 {
+                    if (timeStartedPlaying != DateTime.MinValue)
+                    {
+                        var state = info.Elements().Where(e => e.Attribute("Name").Value == "State").FirstOrDefault()?.Value ?? "";
+                        var secondsSinceStart = Math.Round((DateTime.Now - timeStartedPlaying).TotalSeconds);
+                        if (state == "2")
+                        {
+                            timeStartedPlaying = DateTime.MinValue;
+                            if (secondsSinceStart >= 10)
+                            {
+                                logger.Info($"Waited {secondsSinceStart}s to start");
+                                var activeLogFilePath = "/home/brian/.jriver/Media Center 31/Log.txt";
+                                var copyLogFileName = "JR-" + DateTime.Now.ToString("HHmm") + ".txt";
+                                var copyLogFilePath = Config.FilePath(copyLogFileName);
+                                if (System.IO.File.Exists(activeLogFilePath))
+                                {
+                                    logger.Info($"Preserve JR log file to {copyLogFilePath}");
+                                    Task.Run(() =>
+                                    {
+                                        //  Allow time for the log to flush buffering
+                                        Thread.Sleep(30000);
+                                        System.IO.File.Copy(activeLogFilePath, copyLogFilePath, true);
+                                        logger.Info($"Preserved JR log file to {copyLogFilePath}");
+                                    });
+                                }
+                            }
+                        }
+                        else if (secondsSinceStart > 2)
+                        { 
+                            foreach (var status in info.Elements().Where(e => e.Attribute("Name").Value == "Status"))
+                            {
+                                status.Value = $"Waiting {secondsSinceStart}s";
+                            }
+                        }
+                    }
+
                     //  Re-write the URL for the album image from a MCWS URL to the Avid MVC URL (which will fetch the same content).
                     //  This avoids problems in the client browser that will reject the MCWS URL for a "cross-site scripting" security risk
                     foreach (var xImageUrl in info.Elements().Where(e => e.Attribute("Name").Value == "ImageURL"))
                     {
                         xImageUrl.Value = xImageUrl.Value.Replace("MCWS/v1/File/GetImage?File=", "/Music/GetAlbumImage?id=");
                     }
-				}
+                }
 				info.Save(writer);
+            }
+
+            if (timeStartedPlaying != DateTime.MinValue)
+            {
+                if ((DateTime.Now - timeStartedPlaying).TotalSeconds > 2)
+                {
+                    try
+                    {
+                        var diagnosticScript = Config.FilePath("OnSlowResponse.sh");
+                        if (System.IO.File.Exists(diagnosticScript))
+                        {
+                            using (Process myProcess = new Process())
+                            {
+                                myProcess.StartInfo.FileName = diagnosticScript;
+                                myProcess.StartInfo.UseShellExecute = false;
+                                myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                                myProcess.StartInfo.CreateNoWindow = true;
+                                myProcess.StartInfo.RedirectStandardOutput = true;
+
+                                myProcess.Start();
+
+                                string result = myProcess.StandardOutput.ReadToEnd();
+                                myProcess.WaitForExit();
+                                logger.Info($"OnSlowResponse: '\n{result}'");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.ToString());
+                    }
+                }
             }
             return this.Content(writer.ToString(), @"text/xml", writer.Encoding);
         }
@@ -115,6 +185,10 @@ namespace Avid5.Net.Controllers
             string url)
         {
             logger.Info($"MCWS {url}");
+            if (url.Contains("PlayByKey"))
+            {
+                timeStartedPlaying = DateTime.Now;
+            }
             XDocument doc = JRMC.GetXml(JRMC.Url + url);
             return this.Content(doc == null ? "" : doc.ToString(), @"text/xml");
         }
