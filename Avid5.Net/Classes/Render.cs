d2b077ec0fad282c3ad5e6ac92ffef418a7458af
brianavid <collins.bj@gmail.com>
2024-06-18 14:13:24 +0100

    Prevent use of old cached CSS file when the file has changed

--- Avid5.Net/Classes/Render.cs
+++ Avid5.Net/Classes/Render.cs
@@ -18,12 +18,25 @@ public static class Render
     /// changes in the source file result in different (separately cached) URLs.
     /// </summary>
     /// <param name="scriptName"></param>
-    /// <param name="request"></param>
     /// <returns></returns>
     public static string Script(
         string scriptName)
     {
         return string.Format("<script src='/js/{0}.js?x={1}' type='text/javascript'></script>",
             scriptName, (new System.IO.FileInfo(Path.Combine(Config.ContentRootPath, "wwwroot", "js", scriptName + ".js"))).LastWriteTime.ToString("HHmmss"));
+    }
+
+    /// <summary>
+    /// Generate a link tag for a local URL of the specified CSS file name name, which will be in the "css" directory.
+    /// Append an unused argument whose value is the timestamp of the real source file for the script URL, so that 
+    /// changes in the source file result in different (separately cached) URLs.
+    /// </summary>
+    /// <param name="stylesheetName"></param>
+    /// <returns></returns>
+    public static string Stylesheet(
+        string stylesheetName)
+    {
+        return string.Format("<link rel=\"stylesheet\" href=\"/css/{0}.css?x={1}\" />",
+            stylesheetName, (new System.IO.FileInfo(Path.Combine(Config.ContentRootPath, "wwwroot", "css", stylesheetName + ".css"))).LastWriteTime.ToString("HHmmss"));
     }
 }
